6eafdb3d0c6f9b7736123bdad4ac41e4e938256a
sanglee <pksll325@gmail.com>
2018-07-24 01:45:13 +0900

    models added to admin

--- admin.py
+++ admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Penalty
+from .models import Introduction
 # Register your models here.
+
+admin.site.register(Penalty)
+admin.site.register(Introduction)
--- models.py
+++ models.py
@@ -3,17 +3,25 @@ from django.db import models
 # Create your models here.
 class Penalty(models.Model):
     SEMESTER_CHOICES = (
-        (1, 'Spring')
+        (1, 'Spring'),
         (2, 'Fall')
     )
     title = models.CharField(max_length=50)
-    date_Penalized = models.DateTimeField()
+    date_penalized = models.DateTimeField()
     semester = models.PositiveIntegerField(choices=SEMESTER_CHOICES)
     points = models.IntegerField(default=0)
+
+    def penalized(self):
+        self.date_penalized = timezone.now()
+        self.save
+
+    def __str__(self):
+        return self.title
+
 
 class Introduction(models.Model):
     text = models.CharField(max_length=200)
     lang = models.CharField(max_length=20)
     date = models.DateTimeField('date joined')
-    site = models.URLField(max_length=200)
+    git_link = models.URLField(max_length=200)
 
