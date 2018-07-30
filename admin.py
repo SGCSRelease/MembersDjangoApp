8cb71bdcd1a22b999b4ae8dea760734b8c86a27a
sanglee <pksll325@gmail.com>
2018-07-30 20:02:13 +0900

    admin fixed #1

--- admin.py
+++ admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 from .models import Penalty
-from .models import Introduction
+from .models import Profile
 # Register your models here.
 
 admin.site.register(Penalty)
-admin.site.register(Introduction)
+admin.site.register(Profile)
