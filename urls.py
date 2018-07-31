5983245aa17effaa0dba5bfccffdc94c39f15c16
sanglee <pksll325@gmail.com>
2018-07-31 13:42:41 +0900

    urls and views added

--- urls.py
+++ urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 # Create your urls here.
 urlpatterns = [
+    path('', views.list, name='list'),
+    path('<int:member_id>', views.profile, name='profile'),
+    path('<int:member_id>/penalty', views.penalty, name='penalty')
 ]
--- views.py
+++ views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
 
+from .models import Penalty
+from .models import Profile
 # Create your views here.
+
+def list(request):
+    return HttpResponse("List of Release Members.")
+
+def profile(request, member_id):
+    try:
+        num_profile = Profile.objects.get(pk=member_id)
+    except Profile.DoesNotExist:
+        raise Http404("No profile is uploaded")
+    return render(request, 'members/profile.html', {'Profile': num_profile})
+
+def penalty(request, member_id):
+    try:
+        num_penalty = Penalty.objects.get(pk=member_id)
+    except Penalty.DoesNotExist:
+        raise Http404("No penalty exists")
+    return render(request, 'members/penalty.html', {'Penalty': num_penalty})
